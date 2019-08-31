cc07bd3d90daf25ba173a99ef0621831825164d6
unknown <tomerpacific@gmail.com>
2019-08-31 20:19:34 +0300

    julOnSale adding header to each image

--- script.js
+++ script.js
@@ -40,14 +40,17 @@ function loadData(categories) {
 	 {
 		let category = categories[index];
 		let div = document.createElement('div');
+		let header = document.createElement('h3');
 		let anchor = document.createElement('a');
 		let image = document.createElement('img');
 
+		header.innerHTML = category.name;
 		image.src = 'https://github.com/TomerPacific/julOnSale/blob/master/assets/' + category.image + '.png?raw=true';
 		image.setAttribute('class', 'category');
 		anchor.href = category.link;
 		anchor.innerHTML = '<img src=' + image.src + ' class="category" alt=' + category.name + '>';
 
+		div.appendChild(header);
 		div.appendChild(anchor);
 		mainDiv.appendChild(div);
 	}
