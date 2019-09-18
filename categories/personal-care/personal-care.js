4f2b1696d17d605d70f1b7ff760e0ee35b6f216f
unknown <tomerpacific@gmail.com>
2019-09-18 19:04:00 +0300

    julOnSale adding personal care category

---
+++ categories/personal-care/personal-care.js
@@ -0,0 +1,96 @@
+let mainDiv = document.getElementById("main");
+let loader = document.getElementsByClassName("loader");
+let dateHeader = document.getElementById('date');
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+const url = "https://jul-on-sale.herokuapp.com/category/personal-care";
+
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.open("GET", url, true);
+xmlHttp.setRequestHeader("Content-Type", "application/json");
+
+xmlHttp.onload = function() {
+	var responseText = xmlHttp.responseText;
+	if (!responseText) {
+		return;
+	}
+
+	let data = parseJson(responseText);
+	if (!data || !data.message) {
+		return;
+	}
+
+	mainDiv.innerHTML = '';
+	
+	loadData(data.message);
+
+	loader[0].style.display = "none";
+};
+
+xmlHttp.onerror = function() {
+	console.log("Error");
+};
+
+xmlHttp.send();
+
+function loadData(items) {
+	for (let index = 0; index < items.length; index++)
+	 {
+		let item = items[index];
+		let div = document.createElement('div');
+		let nameHeader = document.createElement('h3');
+		let priceHeader = document.createElement('h2');
+		let anchor = document.createElement('a');
+		let image = document.createElement('img');
+
+		nameHeader.innerHTML = item.name;
+		image.src = item.image;
+		anchor.href = item.link;
+		priceHeader.innerHTML = item.price;
+
+		image.setAttribute('class', 'item');
+		anchor.innerHTML = '<img src=' + image.src + ' class="item" alt=' + item.name + ' title=' + item.name + '>';
+
+		div.appendChild(nameHeader);
+		div.appendChild(anchor);
+		div.appendChild(priceHeader);
+		mainDiv.appendChild(div);
+	}
+}
+
+function parseJson(json) {
+	let data = null;
+	try {
+		data = JSON.parse(json);
+	} catch (e) {
+		console.error("JSON parse error " + e.message);
+	}
+
+	return data;
+}
+
+function setCurrentDate() {
+	var currentDate = new Date();
+	dateHeader.innerHTML = "For the day of " + months[currentDate.getMonth()] + ' ' + currentDate.getDate() + ', ' + currentDate.getFullYear();
+	dateHeader.style.display = "block";
+}
+
+setCurrentDate();
+
+/* Jquery tab title changer */
+
+$(function() {
+	// Get page title
+  	var pageTitle = $("title").text();
+
+	// Change page title on blur
+	$(window).blur(function() {
+	  $("title").text(" ❤️ U");
+	});
+
+	// Change page title back on focus
+	$(window).focus(function() {
+	  $("title").text(pageTitle);
+	});
+});
