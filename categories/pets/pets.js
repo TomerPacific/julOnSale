ca0d6a75e176b0154749a859076fba46a1882f5a
unknown <tomerpacific@gmail.com>
2019-09-18 19:17:18 +0300

    julOnSale adding no items logic

--- categories/pets/pets.js
+++ categories/pets/pets.js
@@ -35,6 +35,15 @@ xmlHttp.onerror = function() {
 xmlHttp.send();
 
 function loadData(items) {
+
+	if (items.length === 0) {
+		let header = document.createElement('h3');
+		header.innerHTML = "Sorry, no items are on sale 😞";
+		mainDiv.appendChild(header);
+		return;
+	}
+
+
 	for (let index = 0; index < items.length; index++)
 	 {
 		let item = items[index];
